8aa921522168f3e66f8f5dbffb6954134b697235
sarasobral <40602@alunos.isel.ipl.pt>
2017-07-09 13:06:26 +0100

    changes on phase 1

---
+++ trabalho1/SerieDeExercicos1Csharp/SearchDiskFiles/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SearchDiskFiles {
+    class Program {
+        static void Main(string[] args)
+        {
+            var folder = @"C:\Windows\System32";
+            var cts = new CancellationTokenSource();
+
+
+            var task = SearchDiskFiles.Find(folder, "dll", "teste", cts.Token);
+
+            try
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var result = task.Result; // wait for the result
+                watch.Stop();
+
+                Console.WriteLine("Time taken: {0}ms", watch.Elapsed.TotalMilliseconds);
+                Console.WriteLine("Total Files Found: " + result.totalFiles);
+                Console.WriteLine("Extension Files Found: " + result.totalFilesWithExtension);
+                Console.WriteLine("Files Matching: ");
+
+                foreach (var resultFile in result.files)
+                {
+                    Console.WriteLine(resultFile);
+                }
+
+            }
+            catch (AggregateException ex)
+            {
+                ex.Handle(e => {
+                    Console.WriteLine("Exception: " + e.Message);
+                    return true;
+                });
+            }
+
+            Console.WriteLine("Press enter..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ trabalho1/SerieDeExercicos1Csharp/SearchDiskFiles/SearchDiskFiles.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SearchDiskFiles {
+    class SearchResult {
+
+        public int totalFiles;
+        public int totalFilesWithExtension;
+        public ConcurrentBag<String> files = new ConcurrentBag<string>();
+
+    }
+
+    class SearchDiskFiles {
+
+        // Caller must catch the AggregateException
+        public static Task<SearchResult> Find(string folder, string ext,
+            string sequence, CancellationToken token)
+        {
+            var task = Task.Factory.StartNew<SearchResult>(() => {
+                var result = new SearchResult();
+
+                // get all the files in that directory to count
+                var files = Directory.GetFiles(folder);
+
+                result.totalFiles = files.Length; // save the count
+
+                // filter the files by the extension we want (toArray because we want the size)
+                var filesWithExtension = files.Where(f => f.EndsWith("." + ext)).ToArray();
+                result.totalFilesWithExtension = filesWithExtension.Length;
+
+                // if canceled at this point, we dont need to read all the files
+                token.ThrowIfCancellationRequested();
+
+                // used to throttled the IO concurrency in case we have many files
+                SemaphoreSlim semaphore = new SemaphoreSlim(20);
+
+                // read the contents of all the files
+                // we assume there are multiple cores on this system
+                // if not, we should do a sync version
+                Parallel.ForEach(filesWithExtension, (file, loop) => {
+                    semaphore.Wait(token); // cancel the wait if token is signaled
+
+                    using (StreamReader reader = File.OpenText(file))
+                    {
+                        string line = null;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            if (line.Contains(sequence))
+                            {
+                                result.files.Add(file);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (token.IsCancellationRequested)
+                    {
+                        loop.Stop(); // cancel all the remaining loop tasks
+                    }
+
+                    semaphore.Release();
+                });
+
+
+                return result;
+            }, token);
+
+            return task;
+        }
+
+    }
+
+}
