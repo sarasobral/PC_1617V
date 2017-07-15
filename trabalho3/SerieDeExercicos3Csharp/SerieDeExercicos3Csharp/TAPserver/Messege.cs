0550c085cf22f31dabdd2c643f2dc0be348917e7
sarasobral <40602@alunos.isel.ipl.pt>
2017-07-15 12:56:42 +0100

    exercise 2: finished (todo: testing)

---
+++ trabalho3/SerieDeExercicos3Csharp/SerieDeExercicos3Csharp/TAPserver/Messege.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TAPserver {
+    public class Message<T> where T : class {
+
+        private readonly object obj = new object();
+        private LinkedList<T> _messages = new LinkedList<T>();
+        private Taker _taker;
+
+        private readonly long _maxNumberOfMsgs;
+
+        public Message(long maxNumberOfMsgs)
+        {
+            this._maxNumberOfMsgs = maxNumberOfMsgs;
+        }
+
+        public void Put(T msg)
+        {
+            lock (obj)
+            {
+                if (_messages.Count >= _maxNumberOfMsgs)
+                    return;
+
+                if (_taker != null)
+                {
+                    _taker.Messages.AddLast(msg);
+                    if (!_taker._hasMessages)
+                    {
+                        _taker._hasMessages = true;
+                        Monitor.Pulse(obj);
+                    }
+                    return;
+                }
+                _messages.AddLast(msg);
+            }
+        }
+
+        public LinkedList<T> Take()
+        {
+            lock (obj)
+            {
+                if (_taker != null)
+                    throw new InvalidOperationException();
+
+                if (_messages.Count > 0)
+                {
+                    LinkedList<T> ret = _messages;
+                    _messages = new LinkedList<T>();
+                    return ret;
+                }
+
+                Taker t = new Taker();
+                _taker = t;
+                do
+                {
+                    try
+                    {
+                        Monitor.Wait(obj);
+                    }
+                    catch (ThreadInterruptedException)
+                    {
+                        if (t._hasMessages)
+                        {
+                            Thread.CurrentThread.Interrupt();
+                            return t.Messages;
+                        }
+                        _taker = null;
+                        throw;
+                    }
+
+                } while (!t._hasMessages);
+                _taker = null;
+                return t.Messages;
+            }
+        }
+
+        public bool HasMessages()
+        {
+            lock (obj)
+            {
+                return _messages.Count > 0;
+            }
+        }
+
+        private class Taker {
+            public readonly LinkedList<T> Messages = new LinkedList<T>();
+            public bool _hasMessages;
+        }
+    }
+}
---
+++ trabalho3/SerieDeExercicos3Csharp/SerieDeExercicos3Csharp/TAPserver/SyncUtils.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading;
+
+namespace TAPserver {
+    public class SyncUtils {
+        public static int AdjustTimeout(ref int lastTime, ref int timeout)
+        {
+            if (timeout != Timeout.Infinite)
+            {
+                int now = Environment.TickCount;
+                int elapsed = (now == lastTime) ? 1 : now - lastTime;
+                if (elapsed >= timeout)
+                {
+                    timeout = 0;
+                }
+                else
+                {
+                    timeout -= elapsed;
+                    lastTime = now;
+                }
+            }
+            return timeout;
+        }
+
+        public static void Wait(object mlock, object condition)
+        {
+            Wait(mlock, condition, Timeout.Infinite);
+        }
+
+        public static void Wait(object mlock, object condition, int tmout)
+        {
+            if (mlock == condition)
+            {
+                Monitor.Wait(mlock, tmout);
+                return;
+            }
+
+            Monitor.Enter(condition);
+            Monitor.Exit(mlock);
+
+            try
+            {
+                Monitor.Wait(condition, tmout);
+            }
+            finally
+            {
+                Monitor.Exit(condition);
+
+                bool interrupted;
+                EnterUninterruptibly(mlock, out interrupted);
+                if (interrupted) throw new ThreadInterruptedException();
+            }
+        }
+
+        public static void Notify(object mlock, object condition)
+        {
+            if (mlock == condition)
+            {
+                Monitor.Pulse(mlock);
+                return;
+            }
+
+            bool interrupted;
+            EnterUninterruptibly(condition, out interrupted);
+
+            Monitor.Pulse(condition);
+            Monitor.Exit(condition);
+
+            if (interrupted)
+                Thread.CurrentThread.Interrupt();
+        }
+
+        public static void Broadcast(object mlock, object condition)
+        {
+            if (mlock == condition)
+            {
+                Monitor.PulseAll(mlock);
+                return;
+            }
+
+            bool interrupted;
+            EnterUninterruptibly(condition, out interrupted);
+
+            Monitor.PulseAll(condition);
+            Monitor.Exit(condition);
+
+            if (interrupted)
+                Thread.CurrentThread.Interrupt();
+        }
+
+        private static void EnterUninterruptibly(object mlock, out bool interrupted)
+        {
+            interrupted = false;
+            do
+            {
+                try
+                {
+                    Monitor.Enter(mlock);
+                    break;
+                }
+                catch (ThreadInterruptedException)
+                {
+                    interrupted = true;
+                }
+            } while (true);
+        }
+
+    }
+}
