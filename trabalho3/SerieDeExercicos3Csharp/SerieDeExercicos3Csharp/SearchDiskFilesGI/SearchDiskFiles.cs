c824bf56ae85b63fb5c9a42f98fc6e14820ef181
sarasobral <40602@alunos.isel.ipl.pt>
2017-07-15 12:58:01 +0100

    exercise 4: finished (todo: testing)

--- trabalho3/SerieDeExercicos3Csharp/SerieDeExercicos3Csharp/SearchDiskFilesGI/SearchDiskFiles.cs
+++ trabalho3/SerieDeExercicos3Csharp/SerieDeExercicos3Csharp/SearchDiskFilesGI/SearchDiskFiles.cs
@@ -9,21 +9,17 @@ using System.Threading.Tasks;
 
 namespace SearchDiskFilesGI {
     class SearchResult {
-
         public int totalFiles;
         public int totalFilesWithExtension;
         public ConcurrentBag<String> files = new ConcurrentBag<string>();
-
     }
     class SearchDiskFiles {
         // Caller must catch the AggregateException
-        public static Task<SearchResult> Find(string folder, string ext,
-            string sequence, CancellationToken token, IProgress<CustomProgress> progress)
+        public static Task<SearchResult> Find(string folder, string ext, string keyWirk, CancellationToken token, IProgress<CustomProgress> progress)
         {
             var task = Task.Factory.StartNew<SearchResult>(() =>
             {
                 var result = new SearchResult();
-
                 string[] files = null;
                 try
                 {
@@ -36,34 +32,27 @@ namespace SearchDiskFilesGI {
                     // forget subdirectories, just search in the root folder
                     files = Directory.GetFiles(folder);
                 }
-
                 result.totalFiles = files.Length; // save the count
-
                 // filter the files by the extension we want (toArray because we want the size)
                 var filesWithExtension = files.Where(f => f.EndsWith("." + ext)).ToArray();
                 result.totalFilesWithExtension = filesWithExtension.Length;
-
                 // if canceled at this point, we dont need to read all the files
                 token.ThrowIfCancellationRequested();
-
                 // used to throttled the IO concurrency in case we have many files
                 SemaphoreSlim semaphore = new SemaphoreSlim(20);
-
                 // read the contents of all the files
                 // we assume there are multiple cores on this system
                 // if not, we should do a sync version
-                Parallel.ForEach(filesWithExtension, (file, loop) =>
+                Parallel.ForEach(filesWithExtension, (file, loopState) =>
                 {
                     semaphore.Wait(token); // cancel the wait if token is signaled
-
                     var contains = false;
-
                     using (StreamReader reader = File.OpenText(file))
                     {
                         string line = null;
                         while ((line = reader.ReadLine()) != null)
                         {
-                            if (line.Contains(sequence))
+                            if (line.Contains(keyWirk))
                             {
                                 contains = true;
                                 result.files.Add(file);
@@ -71,22 +60,14 @@ namespace SearchDiskFilesGI {
                             }
                         }
                     }
-
                     var state = new CustomProgress(result.totalFilesWithExtension, contains ? file : null);
                     progress.Report(state); // will run in the ui thread
-
                     if (token.IsCancellationRequested)
-                    {
-                        loop.Stop(); // cancel all the remaining loop tasks
-                    }
-
+                        loopState.Stop(); // cancel all the remaining loop tasks
                     semaphore.Release();
                 });
-
-
                 return result;
             }, token);
-
             return task;
         }
     }
